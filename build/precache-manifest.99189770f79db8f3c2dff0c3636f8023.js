719dc97c784a74a03d1518d0d569ee68b646f4ca
Nick Romano <njr7romano@yahoo.com>
2020-07-06 17:23:57 -0400

    Created build for project

---
+++ build/precache-manifest.99189770f79db8f3c2dff0c3636f8023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f17646a79e82f9d33d9e0c79fac5aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19aa776aeedb12acfc55",
+    "url": "/static/css/main.851b00f5.chunk.css"
+  },
+  {
+    "revision": "cd8893f209cf25a93d9f",
+    "url": "/static/js/2.a2938cf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2938cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19aa776aeedb12acfc55",
+    "url": "/static/js/main.78abe084.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  }
+]);
